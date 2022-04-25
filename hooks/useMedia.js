53d87ce32e0299d994e73a48f2d0caea246778dc
demosofa <thientndgcd18746@fpt.edu.vn>
2022-04-25 08:55:34 +0700

    fix useMedia not working properly

--- hooks/useMedia.js
+++ hooks/useMedia.js
@@ -2,8 +2,18 @@ import { useState, useEffect, useRef } from "react";
 import Devices from "../helpers/Devices";
 
 export default function useMedia(min = 768, max = 1023) {
-  const [device, setDevice] = useState(Devices.pc);
+  const [device, setDevice] = useState();
   const matchMediaRef = useRef();
+
+  useEffect(() => {
+    const initialValue = () =>
+      (window.innerWidth >= max && Devices.pc) ||
+      (window.innerWidth >= min && Devices.tablet) ||
+      Devices.phone;
+    setDevice(() => {
+      return initialValue();
+    });
+  }, [min, max]);
 
   useEffect(() => {
     matchMediaRef.current = window.matchMedia(
