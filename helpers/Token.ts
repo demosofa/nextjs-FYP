56a9afad30fa01fbb0a6c6fcd31c7e8e129baa05
demosofa <thientndgcd18746@fpt.edu.vn>
2022-09-27 18:32:55 +0700

    update AuthController for reseting password feature, update UI for pages and component relating to Form and NavBar; add pages for reseting password

---
+++ helpers/Token.ts
@@ -0,0 +1,36 @@
+import { sign, verify, SignOptions, VerifyOptions } from "jsonwebtoken";
+
+export default class Token {
+  accessToken: string;
+  refreshToken: string;
+  constructor(payload: string | object | Buffer) {
+    this.accessToken = sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "5m",
+    });
+    this.refreshToken = sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+  }
+  static signToken(
+    payload: string | object | Buffer,
+    secret = "",
+    options: SignOptions
+  ) {
+    return sign(payload, process.env.SECRET_KEY + secret, options);
+  }
+  static verifyToken(
+    token: string,
+    secret = "",
+    options: VerifyOptions & {
+      complete: true;
+    }
+  ) {
+    return verify(token, process.env.SECRET_KEY + secret, options);
+  }
+  static verifyAccessToken(token: string) {
+    return verify(token, process.env.ACCESS_TOKEN_SECRET);
+  }
+  static verifyRefreshToken(token: string) {
+    return verify(token, process.env.REFRESH_TOKEN_SECRET);
+  }
+}
--- helpers/blacklist.ts
+++ helpers/blacklist.ts
@@ -1,9 +1,8 @@
-/**@type {{accountId: string, expire: Date}[]} */
-let blacklist = [];
+let blacklist: { accountId: string; expire: Date }[] = [];
 
 export default {
   blacklist,
-  isInBlackList: (accountId) => {
+  isInBlackList: (accountId: string) => {
     const index = blacklist.findIndex((item) => item.accountId === accountId);
     if (index === -1) return 0;
     const expireTime = blacklist[index].expire;
@@ -12,13 +11,16 @@ export default {
       return -1;
     } else return 1;
   },
-  addToBlackList: (accountId, expire = null) => {
+  addToBlackList: (accountId: string, expire?: Date) => {
     const index = blacklist.findIndex((item) => item.accountId === accountId);
-    if (index !== -1) return;
+    if (index !== -1) {
+      blacklist[index].expire = expire;
+      return;
+    }
     blacklist.push({ accountId, expire });
     return;
   },
-  removeFromBlackList: (accountId) => {
+  removeFromBlackList: (accountId: string) => {
     const index = blacklist.findIndex((item) => item.accountId === accountId);
     if (index === -1) return;
     blacklist.splice(index, 1);
