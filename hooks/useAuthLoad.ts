851f83994d5a4796097bb3550a893f97fe0e4e66
demosofa <thientndgcd18746@fpt.edu.vn>
2022-11-22 17:25:55 +0700

    update useAuthHook to make its more optimize

--- hooks/useAuthLoad.ts
+++ hooks/useAuthLoad.ts
@@ -25,16 +25,21 @@ export default function useAuthLoad({
     deps,
     callback: async (axiosInstance) => {
       let accessToken = expireStorage.getItem("accessToken");
-      if (!accessToken) {
-        setLoading(false);
-        setLoggined(false);
-        setAuthorized(null);
-        return;
-      } else setLoggined(true);
-
       retryAxios(axiosInstance);
       try {
-        const { role } = decoder(accessToken) as { role: string };
+        const { role, exp } = decoder(accessToken) as {
+          role: string;
+          exp: number;
+        };
+        if (Date.now() >= exp * 1000) throw new Error();
+
+        if (!role) {
+          setLoading(false);
+          setLoggined(false);
+          setAuthorized(null);
+          return;
+        } else setLoggined(true);
+
         if (!roles.includes(role)) {
           setLoading(false);
           setAuthorized(null);
@@ -49,14 +54,18 @@ export default function useAuthLoad({
             const { role } = decoder(accessToken) as { role: string };
             setAuthorized(role);
           })
-          .catch((res) => setError(res));
+          .catch(({ response }) => {
+            if (response.status === 401) {
+              localStorage.clear();
+              window.location = response.data;
+            } else setError(response.message);
+          });
       }
 
       if (cb) {
         axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
         await cb(axiosInstance);
       }
-      return;
     },
   });
   return { loading, isLoggined, isAuthorized, error, axiosInstance };
