da1f26d2c4cf02d1f6f904ad1fec6f3a7c3b168c
demosofa <thientndgcd18746@fpt.edu.vn>
2023-05-06 11:47:24 +0700

    testing commit

--- frontend/redux/reducer/variantSlice.js
+++ frontend/redux/reducer/variantSlice.js
@@ -1,35 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 /** @type {{ id: string, name: string, options: string[] }[]} */
-export const initialState = [];
+export const initialState = []
 
 const variant = createSlice({
-  initialState,
-  name: "variant",
-  reducers: {
-    addVariant(state) {
-      let check = false;
-      let unique = { ...initialState.concat()[0] };
-      while (!check) {
-        unique.id = Math.random();
-        let existed = state.filter((item) => item.id === unique.id);
-        if (existed.length === 0) check = true;
-      }
-      return [...state, unique];
-    },
-    editVariant(state, { payload }) {
-      const { index, ...props } = payload;
-      state[index] = { ...state[index], ...props };
-    },
-    deleteVariant(state, { payload }) {
-      return state.filter((_, index) => index !== payload);
-    },
-    deleteAllVariant() {
-      return [];
-    },
-  },
-});
+	initialState,
+	name: 'variant',
+	reducers: {
+		addVariant(state) {
+			let check = false
+			let unique = { ...initialState.concat()[0] }
+			while (!check) {
+				unique.id = Math.random()
+				let existed = state.filter((item) => item.id === unique.id)
+				if (existed.length === 0) check = true
+			}
+			return [...state, unique]
+		},
+		editVariant(state, { payload }) {
+			const { index, ...props } = payload
+			state[index] = { ...state[index], ...props }
+		},
+		deleteVariant(state, { payload }) {
+			return state.filter((_, index) => index !== payload)
+		},
+		deleteAllVariant() {
+			return []
+		}
+	}
+})
 
-export default variant.reducer;
-export const { addVariant, editVariant, deleteVariant, deleteAllVariant } =
-  variant.actions;
+export default variant.reducer
+export const { addVariant, editVariant, deleteVariant, deleteAllVariant } = variant.actions
--- next.config.js
+++ next.config.js
@@ -1,33 +1,36 @@
-const path = require("path");
+const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "sass")],
-  },
-  // modularizeImports: {
-  //   "react-icons/?(((\\w*)?/?)*)": {
-  //     transform: "react-icons/{{matches.[1]}}",
-  //     skipDefaultConversion: true,
-  //   },
-  // },
-};
+	reactStrictMode: true,
+	eslint: {
+		dirs: ['pages', 'frontend', 'shared', 'backend'],
+	},
+	images: {
+		domains: ['res.cloudinary.com'],
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'sass')],
+	},
+	// modularizeImports: {
+	//   "react-icons/?(((\\w*)?/?)*)": {
+	//     transform: "react-icons/{{matches.[1]}}",
+	//     skipDefaultConversion: true,
+	//   },
+	// },
+}
 
-const runtimeCaching = require("next-pwa/cache");
+const runtimeCaching = require('next-pwa/cache')
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  runtimeCaching,
-  // buildExcludes: ["/chunks/.*$/"],
-});
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === 'development',
+	runtimeCaching,
+	// buildExcludes: ["/chunks/.*$/"],
+})
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = withBundleAnalyzer(withPWA(nextConfig));
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
